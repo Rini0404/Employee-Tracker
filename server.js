2e8f4621c4e84fef935fdff00f447b27e21cf96e
Rene Ortega <reneortega2468@gmail.com>
2022-05-03 10:00:36 -0400

    working on server.js, pushing my inquirer there.

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const inquirer = require('inquirer');
+// const db = require('../db/connections');
+
+// const { check } = require('../helpers/validation');
+
+
 // middleware
 app.use (express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -17,5 +23,98 @@ const connection = mysql.createConnection(
   },
 
   console.log(`connected to Employess DataBase! `)
+
 );
 
+  startPrompt();
+// ++++++++++++++++++++++ Starting Prompt+++++++++++++++++++++++++++
+  function startPrompt () {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'How may we begin?',
+          choices: [
+            'View Roles',
+            'View employees',
+            'Add a department to your organization',
+            'Add Role',
+            'Add employee',
+            'Update Employee Role',
+            'Exit',
+          ]
+      }
+    ]).then(function(val) {
+        switch (val.choice){
+          case 'View Employees':
+            viewEmployees(); //call the function to see employees
+          break;
+          // new case
+            case 'View Roles':
+              viewRoles(); //call the function to see roles 
+            break;
+          // new case
+            case 'View Departments':
+              viewDepartmants(); //call the function to see departments
+            break;
+          // new case case addStuff 
+            case 'Add new Employee':
+              addEmployee();
+            break;
+          // add
+            case 'Update Employee':
+              updateEmployee();
+            break;
+          // add
+            case 'Add Role':
+              addRole();
+            break;
+          // add
+            case 'Add Department':
+              addDepartment();
+            break;
+          // add
+            case 'Update Employee':
+              updateEmployee();
+            break;
+          // add
+
+        }
+    })
+  }
+// view Emps
+
+function viewEmployees () {
+  connection.query('SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, '', e.last_name) 
+  )
+}
+
+
+
+
+
+
+  // const departments = () => {
+  //   inquirer.prompt([
+  //     {
+  //       type: 'input',
+  //       name: 'department',
+  //       message: 'What department would you like to access?',
+  //       // validate: stringCheck, 
+  //       // filter the input to trim `
+  //         filter: (input) => {
+  //           return input.trim();
+  //         },
+  //     }
+  //   ]).then((answers) => {
+  //     const sql = `INSERT INTO departments (name) VALUES (?)`;
+  //       const par = [answers.departments];
+  //         db.query(sql, par, (err, answers) => {
+  //           (err) ? console.log(err) : console.log(`${par} added to the new Departments`)
+  //         })
+  //   })
+  // }
+
+  // const addRole = [{
+  //   type: 'input'
+  // }];
--- util/index.js
+++ util/index.js
@@ -1,50 +1,50 @@
-const { prompt } = require('inquirer');
-const db = require('../db/connections');
+// const { prompt } = require('inquirer');
+// const db = require('../db/connections');
 
-const { check } = require('../helpers/validation');
-
-
+// const { check } = require('../helpers/validation');
 
 
-const menu = [
-    {
-      type: 'list',
-      name: 'main',
-      message: 'How may we begin?',
-        choices: [
+
+
+// const menu = [
+//     {
+//       type: 'list',
+//       name: 'main',
+//       message: 'How may we begin?',
+//         choices: [
           
-          'View Roles',
-          'View employees',
-          'Add a department to your organization',
-          'Add Role',
-          'Add employee',
-          'Update Employee Role',
-          'Exit',
-        ], 
-    },
-  ];
+//           'View Roles',
+//           'View employees',
+//           'Add a department to your organization',
+//           'Add Role',
+//           'Add employee',
+//           'Update Employee Role',
+//           'Exit',
+//         ], 
+//     },
+//   ];
 
-  const departments = () => {
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'department',
-        message: 'What department would you like to access?',
-        // validate: stringCheck, 
-        // filter the input to trim `
-          filter: (input) => {
-            return input.trim();
-          },
-      }
-    ]).then((answers) => {
-      const sql = `INSERT INTO departments (name) VALUES (?)`;
-        const par = [answers.departments];
-          db.query(sql, par, (err, answers) => {
-            (err) ? console.log(err) : console.log(`${par} added to the new Departments`)
-          })
-    })
-  }
+//   const departments = () => {
+//     inquirer.prompt([
+//       {
+//         type: 'input',
+//         name: 'department',
+//         message: 'What department would you like to access?',
+//         // validate: stringCheck, 
+//         // filter the input to trim `
+//           filter: (input) => {
+//             return input.trim();
+//           },
+//       }
+//     ]).then((answers) => {
+//       const sql = `INSERT INTO departments (name) VALUES (?)`;
+//         const par = [answers.departments];
+//           db.query(sql, par, (err, answers) => {
+//             (err) ? console.log(err) : console.log(`${par} added to the new Departments`)
+//           })
+//     })
+//   }
 
-  const addRole = [{
-    type: 'input'
-  }];
+//   const addRole = [{
+//     type: 'input'
+//   }];
